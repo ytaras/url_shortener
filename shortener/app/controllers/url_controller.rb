e745e0aa3ffe11f7fb390c0a12f9c18a532f3128
Yura Taras <yura.taras@gmail.com>
2013-09-03 19:43:18 +0300

    Added controller

---
+++ shortener/app/controllers/url_controller.rb
@@ -0,0 +1,24 @@
+class UrlController < ApplicationController
+
+  def index
+    @urls = Url.all
+    respond_to do |f|
+      f.html
+    end
+  end
+
+  def create
+    @created = Url.generate(params[:url]) # TODO XSS possible
+    redirect_to url_url(@created)
+  end
+
+  def show
+    @url = Url.load(params[:id])
+    respond_to do |f|
+      f.html
+    end
+  end
+
+  def new
+  end
+end
--- shortener/config/routes.rb
+++ shortener/config/routes.rb
@@ -1,4 +1,5 @@
 Shortener::Application.routes.draw do
+  resource :urls, except: [:update, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -39,7 +40,7 @@ Shortener::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
