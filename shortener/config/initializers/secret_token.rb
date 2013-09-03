f6571b15a1e835841f6a41d18b7063f8dfa163ed
Yura Taras <yura.taras@gmail.com>
2013-09-03 19:20:55 +0300

    Init

---
+++ shortener/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shortener::Application.config.secret_key_base = '141a087632f143a49dd7e4f47c9ededa18d676d4f76fb8e70c4e6a67f2aeeb53b1378b81bf6b58f660d8d9a34bffae22cb6bf56052e7ade1107fc84ecfe60833'
